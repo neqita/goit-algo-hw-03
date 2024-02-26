5172317c5167c8825dd076bc78e1fab0fbc695af
asus <nekit5951@gmail.com>
2024-02-26 17:23:11 +0200

    add file

---
+++ #1.py
@@ -0,0 +1,20 @@
+from datetime import datetime as dtdt
+
+
+def get_days_from_today(date):
+    try:
+        input_date = dtdt.strptime(date, '%Y-%m-%d')
+
+        current_date = dtdt.today()
+
+        difference = (current_date - input_date).days
+
+        return difference
+    except ValueError:
+        # Обробка винятку, якщо рядок дати має неправильний формат
+        return "Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'."
+
+
+today = dtdt.today().strftime('%Y-%m-%d')
+result = get_days_from_today("2018-07-11")
+print(f"Різниця у днях: {result}")
